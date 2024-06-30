05dec06d9f44bbddf4b5b4395a9cc0f7db343303
ayathree <nobanitaayathree333@gmail.com>
2024-06-30 23:31:57 +0600

    added bannar

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,19 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './Page/Root';
+import Home from './Page/Home';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
   },
 ]);
 
